14db0a25449b123ba50f3e8e02cae20e90635d3e
Gavin Mikesell <gavinmikesell@gmail.com>
2024-10-02 13:03:15 -0600

    class prep 3

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        Job job1 = new Job();
+        job1._Title = "Manager";
+        job1._company = "McDonalds";
+        job1._startYear = 2010;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._Title = "Salseman";
+        job2._company = "Apple";
+        job2._startYear = 2020;
+        job2._endYear = 2023;
+
+        Resume myResume = new Resume();
+        myResume._name = "Gavin Mikesell";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
